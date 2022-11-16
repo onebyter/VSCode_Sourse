4ec13a33088625e2e699a3d9b92fde2ee2399a49
2926201572@qq.com <20022002WJYwjy>
2022-11-16 21:49:40 +0800

    计算机能力挑战赛20第一题

---
+++ 计算机能力挑战赛/Excel列数计算.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+using namespace std;
+
+char num[200];
+
+void init()
+{
+	num['A'] = 1;
+
+	for (int i = 1; i <= 25; i++)
+		num['A' + i] = num['A'] + i;
+}
+
+int change(string s)
+{
+	int x = 0;
+
+	for (char c : s)
+		x = x * 26 + num[c];
+
+	return x;
+}
+
+int main()
+{
+	string s, t;
+	cin >> s >> t;
+	init();
+	int x = change(s);
+	int y = change(t);
+	cout << y - x - 1;
+	return 0;
+}
