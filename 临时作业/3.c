a475a355b02f3f54f1ea3d85a2741aeaf9739742
2926201572@qq.com <20022002WJYwjy>
2022-11-18 00:31:31 +0800

    没有修改

--- 临时作业/3.c
+++ 临时作业/3.c
@@ -1,13 +1,10 @@
 #include<stdio.h>
 
-int main() {
-	int n;
-	printf("请输入一个数\n");
-	scanf("%d", &n);
-	long long s = 1;
-	for (int i = 1; i <= n; i++) {
-		s *= i;
-	}
-	printf("结果为：%lld\n", s);
+int main()
+{
+	unsigned int x = 134;
+	unsigned int y = 246;
+	printf("%u ", x - y);
+	printf("%d ", 0x90);
 	return 0;
 }
